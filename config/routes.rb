24cd5c4886a1d2c2c5ee2816dbc4d8da683e57cd
ShunTakahashi <tshun.1996@icloud.com>
2019-10-26 17:27:40 +0900

    Add seed date

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
-  resources :users
-  resources :sessions, only: :new
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: 'テスト太郎',
+  email: 'test@test.com',
+  password: '12345678',
+  )
