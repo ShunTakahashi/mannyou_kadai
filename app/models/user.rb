e6a8a9faf2fd3f2300c0872095cd6972f7b3fe06
ShunTakahashi <tshun.1996@icloud.com>
2019-11-04 17:59:29 +0900

    Add user validates skip(edit)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_many :tasks
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, on: :create
+  validates :email, presence: true, uniqueness: true, on: :create
   has_secure_password
-  validates :password, presence: true, length: {minimum: 8}
-
+  validates :password, presence: true, length: {minimum: 8}, on: :create
 end
