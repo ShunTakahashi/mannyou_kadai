2e8a694a3e0841fef6e5b6d9c19c98ae40439f43
ShunTakahashi <tshun.1996@icloud.com>
2019-11-09 14:53:14 +0900

    Add model Labeling

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_053456) do
+ActiveRecord::Schema.define(version: 2019_11_09_054904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labelings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "task_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "labels", force: :cascade do |t|
     t.string "label_name"
